ad0fde86ab532b4dfb4e74a5906ab3bcacb614a9
vvouteiral <121929814+vvouteiral@users.noreply.github.com>
2023-01-09 11:55:39 +0100

    First commit

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImplv.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImplv implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImplv() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
